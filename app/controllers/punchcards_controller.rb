b8d173dac907d8abd685f7cb46829c495a45af1a
Jaime Alvarez <jaime_andres@unitec.edu>
2016-03-11 04:46:24 +0000

    Add new action

--- app/controllers/punchcards_controller.rb
+++ app/controllers/punchcards_controller.rb
@@ -3,6 +3,9 @@ class PunchcardsController < ApplicationController
     before_action :authenticate_user!
     before_action :find_punchcard, only: [:show, :edit, :update, :destroy]
     
+    def new
+        @card = Punchcard.new
+    end
     
     private
     
