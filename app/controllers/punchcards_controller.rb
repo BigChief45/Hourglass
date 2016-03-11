c2530152e4e1c03d4dae6ecddcbd64a984ab3423
Jaime Alvarez <jaime_andres@unitec.edu>
2016-03-11 05:05:42 +0000

    Add create action

--- app/controllers/punchcards_controller.rb
+++ app/controllers/punchcards_controller.rb
@@ -3,13 +3,33 @@ class PunchcardsController < ApplicationController
     before_action :authenticate_user!
     before_action :find_punchcard, only: [:show, :edit, :update, :destroy]
     
+    def index
+       @cards = current_user.punchcards.all 
+    end
+    
     def new
-        @card = Punchcard.new
+        @card = current_user.punchcards.build
+    end
+    
+    def create
+       @card = current_user.punchcards.build(punchcard_params)
+       
+       respond_to do |format|
+            if @card.save
+                format.html { redirect_to punchcards_path, :flash => { :success => 'Punchcard created successfully.' } }
+            else
+                format.html { render 'new', :flash => { :danger => 'Error creating punchcard.' } }
+            end
+        end
     end
     
     private
     
         def find_punchcard
+        end
+        
+        def punchcard_params
+            params.require(:punchcard).permit(:name, :description, :icon, :user_id)
         end
     
 end
