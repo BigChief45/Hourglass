5abf058be23a1ffeeaa01124c7c82c7b87898351
Jaime Alvarez <jaime_andres@unitec.edu>
2016-03-11 04:47:07 +0000

    Migrate db

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160311034439) do
+ActiveRecord::Schema.define(version: 20160311043906) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "punchcards", force: :cascade do |t|
+    t.string   "name"
+    t.string   "description"
+    t.string   "icon"
+    t.integer  "user_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
