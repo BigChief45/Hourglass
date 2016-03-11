c4021bba7eedd2d2c1d85b8c64920bd884b8d224
Jaime Alvarez <jaime_andres@unitec.edu>
2016-03-11 04:27:42 +0000

    Add auth to punchcards controller

--- app/controllers/punchcards_controller.rb
+++ app/controllers/punchcards_controller.rb
@@ -1,2 +1,12 @@
 class PunchcardsController < ApplicationController
+    
+    before_action :authenticate_user!
+    before_action :find_punchcard, only: [:show, :edit, :update, :destroy]
+    
+    
+    private
+    
+        def find_punchcard
+        end
+    
 end
