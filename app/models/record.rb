9826f1944bf18d7bc51587950dae73fd5f45831b
Jaime Alvarez <jaime_andres@unitec.edu>
2016-03-30 01:43:05 +0000

    Validate presence of hours in records

--- app/models/record.rb
+++ app/models/record.rb
@@ -2,6 +2,7 @@ class Record < ActiveRecord::Base
     belongs_to :punchcard
     
     validates :date, :presence => true
+    validates :hours, :presence => true
     validates :description, :presence => true
     
 end
