74213914d969c5b13832cf5b50cca3c0ad5b9656
Jaime Alvarez <jaime_andres@unitec.edu>
2016-03-30 01:51:27 +0000

    Validate uniqueness of record dates

--- app/models/record.rb
+++ app/models/record.rb
@@ -2,6 +2,8 @@ class Record < ActiveRecord::Base
     belongs_to :punchcard
     
     validates :date, :presence => true
+    validates :date, :uniqueness => { scope: :punchcard_id }
+    
     validates :hours, :presence => true
     validates :description, :presence => true
     
