3496d047abdfe5a7f36ca663c454e22d2f109f5b
Jaime Alvarez <jaime_andres@unitec.edu>
2016-06-20 05:46:41 +0000

    Add previous and next pagers to records

--- app/models/record.rb
+++ app/models/record.rb
@@ -7,4 +7,14 @@ class Record < ActiveRecord::Base
     validates :hours, :presence => true
     validates :description, :presence => true
     
+    
+    def next
+        self.class.where("id > ? AND punchcard_id = ?", id, self.punchcard_id).first
+    end
+
+    def previous
+        self.class.where("id < ? AND punchcard_id = ?", id, self.punchcard_id).last
+    end
+
+    
 end
