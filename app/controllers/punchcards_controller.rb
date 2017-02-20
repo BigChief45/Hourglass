667767a1d190008d1927b055bd984a30490f8861
Jaime Alvarez <jaime_andres@unitec.edu>
2017-02-20 08:41:35 +0000

    Change to 2 space indentation

--- app/controllers/punchcards_controller.rb
+++ app/controllers/punchcards_controller.rb
@@ -1,67 +1,67 @@
 class PunchcardsController < ApplicationController
-    
+
     before_action :authenticate_user!
     before_action :find_punchcard, only: [:show, :edit, :update, :destroy]
-    
+
     def index
-        @cards = current_user.punchcards.all
+      @cards = current_user.punchcards.all
     end
-    
+
     def new
-        @card = current_user.punchcards.build
+      @card = current_user.punchcards.build
     end
-    
+
     def show
-        @records = @card.records.all.order("created_at DESC").limit(5)
-        
-        # Records Pagination
-        #@records = @records.paginate(:page => params[:page], :per_page => 1)
-        
-        respond_to do |format|
-            format.js
-        end
+      @records = @card.records.all.order("created_at DESC").limit(5)
+
+      # Records Pagination
+      #@records = @records.paginate(:page => params[:page], :per_page => 1)
+
+      respond_to do |format|
+        format.js
+      end
     end
-    
+
     def create
-       @card = current_user.punchcards.build(punchcard_params)
-       
-       respond_to do |format|
-            if @card.save
-                format.html { redirect_to punchcards_path, :flash => { :success => 'Punchcard created successfully.' } }
-            else
-                format.html { render 'new', :flash => { :danger => 'Error creating punchcard.' } }
-            end
+     @card = current_user.punchcards.build(punchcard_params)
+
+     respond_to do |format|
+        if @card.save
+          format.html { redirect_to punchcards_path, :flash => { :success => 'Punchcard created successfully.' } }
+        else
+          format.html { render 'new', :flash => { :danger => 'Error creating punchcard.' } }
         end
+      end
     end
-    
+
     def edit
     end
-    
+
     def update
-        respond_to do |format|
-            if @card.update(punchcard_params)
-                format.html { redirect_to punchcards_path, notice: 'Punchcard was successfully updated.' }
-            else
-                format.html { render :edit }
-            end
+      respond_to do |format|
+        if @card.update(punchcard_params)
+          format.html { redirect_to punchcards_path, notice: 'Punchcard was successfully updated.' }
+        else
+          format.html { render :edit }
         end
+      end
     end
-    
+
     def destroy
-        @card.destroy
-        respond_to do |format|
-            format.html { redirect_to punchcards_path, notice: 'Punchcard was successfully deleted.' }
-        end
+      @card.destroy
+      respond_to do |format|
+        format.html { redirect_to punchcards_path, notice: 'Punchcard was successfully deleted.' }
+      end
     end
-    
+
     private
-    
-        def find_punchcard
-            @card = Punchcard.find(params[:id])
-        end
-        
-        def punchcard_params
-            params.require(:punchcard).permit(:name, :description, :icon, :user_id)
-        end
-    
+
+      def find_punchcard
+        @card = Punchcard.find(params[:id])
+      end
+
+      def punchcard_params
+        params.require(:punchcard).permit(:name, :description, :icon, :user_id)
+      end
+
 end
--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -1,104 +1,104 @@
 class RecordsController < ApplicationController
     before_action :authenticate_user!
-    
+
     before_action :find_punchcard, except: [:search_record, :todays_report]
     before_action :find_record, only: [:show, :edit, :update, :destroy]
-    
+
     def index
-        # Ransack Search
-        @q = @card.records.ransack(params[:q])
-        @records = @q.result(distinct: true).order("created_at DESC")
-        
-        # Pagination
-        @records = @records.paginate(:page => params[:page], :per_page => 10)
-        
-        # JSON Record data For Cal Heat Map
-        @json_records = @card.records
-        
-        # We need to convert the available @records data into the corresponding
-        # data format for Cal-heatmap, which is:
-        #
-        # {
-        # 	"timestamp": value,
-        # 	"timestamp2": value2,
-        # 	...
-        # }
-        hash = {}
-        @json_records.each do |r|
-            hash[r.date.to_i] = r.hours
-        end
-        
-        respond_to do |format|
-            # Return the hash with the correct format
-            format.json { render :json => hash }
-            format.html
-        end
+      # Ransack Search
+      @q = @card.records.ransack(params[:q])
+      @records = @q.result(distinct: true).order("created_at DESC")
+
+      # Pagination
+      @records = @records.paginate(:page => params[:page], :per_page => 10)
+
+      # JSON Record data For Cal Heat Map
+      @json_records = @card.records
+
+      # We need to convert the available @records data into the corresponding
+      # data format for Cal-heatmap, which is:
+      #
+      # {
+      # 	"timestamp": value,
+      # 	"timestamp2": value2,
+      # 	...
+      # }
+      hash = {}
+      @json_records.each do |r|
+          hash[r.date.to_i] = r.hours
+      end
+
+      respond_to do |format|
+        # Return the hash with the correct format
+        format.json { render :json => hash }
+        format.html
+      end
     end
-    
+
     def new
-        @record = @card.records.build
+      @record = @card.records.build
     end
-    
+
     def show
     end
-    
+
     def create
-        @record = @card.records.build(record_params)
-       
-        respond_to do |format|
-            if @record.save
-                format.html { redirect_to punchcards_path, :flash => { :success => 'Record punched successfully.' } }
-            else
-                format.html { render 'new', :flash => { :danger => 'Error punching record.' } }
-            end
+      @record = @card.records.build(record_params)
+
+      respond_to do |format|
+        if @record.save
+          format.html { redirect_to punchcards_path, :flash => { :success => 'Record punched successfully.' } }
+        else
+          format.html { render 'new', :flash => { :danger => 'Error punching record.' } }
         end
+      end
     end
-    
+
     def edit
     end
-    
+
     def update
-        respond_to do |format|
-            if @record.update(record_params)
-                format.html { redirect_to [@card, @record], notice: 'Record was successfully updated.' }
-            else
-                format.html { render :edit }
-            end
+      respond_to do |format|
+        if @record.update(record_params)
+          format.html { redirect_to [@card, @record], notice: 'Record was successfully updated.' }
+        else
+          format.html { render :edit }
         end
+      end
     end
-    
+
     def destroy
-        @record.destroy
-        respond_to do |format|
-            format.html { redirect_to punchcards_path, notice: 'Record was successfully deleted.' }
-        end
+      @record.destroy
+      respond_to do |format|
+        format.html { redirect_to punchcards_path, notice: 'Record was successfully deleted.' }
+      end
     end
-    
+
     def search_record
-        # Searches for a record using its date attribute and punchcard.
-        # Used with AJAX calls within CalHeatMap events
-        record = Record.find_by(date: params[:record_date], punchcard_id: params[:pcard_id])
-       
-        respond_to do |format|
-            format.json { render json: record.id }
-        end
+      # Searches for a record using its date attribute and punchcard.
+      # Used with AJAX calls within CalHeatMap events
+      record = Record.find_by(date: params[:record_date], punchcard_id: params[:pcard_id])
+
+      respond_to do |format|
+        format.json { render json: record.id }
+      end
     end
-    
+
     def todays_report
-       @todays_records = Record.where("date >= ?", DateTime.now.to_date.strftime('%Y-%m-%d'))
+      @todays_records = Record.where("date >= ?", DateTime.now.to_date.strftime('%Y-%m-%d'))
     end
-    
+
     private
-    
-        def find_punchcard
-            @card = Punchcard.find(params[:punchcard_id])
-        end
-        
-        def find_record
-            @record = Record.find(params[:id])
-        end
-        
-        def record_params
-           params.require(:record).permit(:date, :hours, :description, :punchcard_id) 
-        end
+
+      def find_punchcard
+        @card = Punchcard.find(params[:punchcard_id])
+      end
+
+      def find_record
+        @record = Record.find(params[:id])
+      end
+
+      def record_params
+        params.require(:record).permit(:date, :hours, :description, :punchcard_id)
+      end
 end
