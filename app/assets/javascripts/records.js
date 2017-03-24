15c94e8666a7e4627d92ffb3311b8803207349ed
Jaime Alvarez <jaime_andres@unitec.edu>
2017-03-24 02:22:40 +0000

    Fix indentation

--- app/assets/javascripts/records.js
+++ app/assets/javascripts/records.js
@@ -1,26 +1,26 @@
 $(function () {
-    $('.datepicker').datetimepicker({
-        showTodayButton: true,
-        useCurrent: true,
-        defaultDate: new Date(),
-        icons: {
-                    time: "fa fa-clock-o",
-                    date: "fa fa-calendar",
-                    up: "fa fa-arrow-up",
-                    down: "fa fa-arrow-down",
-                    previous: 'fa fa-arrow-left',
-                    next: 'fa fa-arrow-right',
-                    today: 'fa fa-calendar-times-o'
-                }
+  $('.datepicker').datetimepicker({
+    showTodayButton: true,
+    useCurrent: true,
+    defaultDate: new Date(),
+    icons: {
+      time: "fa fa-clock-o",
+      date: "fa fa-calendar",
+      up: "fa fa-arrow-up",
+      down: "fa fa-arrow-down",
+      previous: 'fa fa-arrow-left',
+      next: 'fa fa-arrow-right',
+      today: 'fa fa-calendar-times-o'
+    }
+  });
+
+  // RICH TEXT TEXT AREA
+  $('.wysihtml5').each(function(i, elem) {
+    $(elem).wysihtml5({'toolbar': {
+      'fa': true,
+      'color': true
+      }
     });
-    
-    // RICH TEXT TEXT AREA
-    $('.wysihtml5').each(function(i, elem) {
-        $(elem).wysihtml5({'toolbar': {
-            'fa': true,
-            'color': true
-            }
-        });
-    });
-    
+  });
+
 });
