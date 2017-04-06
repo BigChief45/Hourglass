cd1c7e012ed17655c380c75010dea8e8010aae3c
Jaime Alvarez <jaime_andres@unitec.edu>
2017-04-06 09:34:35 +0000

    Fix validations placing mistake

--- spec/models/punchcard_spec.rb
+++ spec/models/punchcard_spec.rb
@@ -7,8 +7,4 @@ describe Punchcard, type: :model do
     expect(build(:punchcard)).to be_valid
   end
 
-  context "validations" do
-    it { is_expected.to validate_presence_of :email }
-    it { is_expected.to validate_confirmation_of :password }
-  end
 end
--- spec/models/users.rb
+++ spec/models/users.rb
@@ -5,4 +5,9 @@ RSpec.describe User, type: :model do
   it "has a valid factory" do
     expect(FactoryGirl.create(:user)).to be_valid
   end
+
+  context "validations" do
+    it { is_expected.to validate_presence_of :email }
+    it { is_expected.to validate_confirmation_of :password }
+  end
 end
