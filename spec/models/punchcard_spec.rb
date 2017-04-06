99e11f48ca02e323528180fe23eb6d764978fbe0
Jaime Alvarez <jaime_andres@unitec.edu>
2017-04-06 09:50:10 +0000

    Add shoulda specs to model specs

--- spec/models/punchcard_spec.rb
+++ spec/models/punchcard_spec.rb
@@ -2,9 +2,12 @@ require 'rails_helper'
 
 describe Punchcard, type: :model do
 
-  # Validate the factory
   it 'has a valid factory' do
     expect(build(:punchcard)).to be_valid
+  end
+
+  context "validations" do
+    it { is_expected.to validate_presence_of :name }
   end
 
 end
--- spec/models/record_spec.rb
+++ spec/models/record_spec.rb
@@ -6,16 +6,10 @@ describe Record, type: :model do
     expect(build(:record)).to be_valid
   end
 
-  it 'is invalid without an hour' do
-    expect(build(:record, hours: nil)).to_not be_valid
-  end
-
-  it 'is invalid without a description' do
-    expect(build(:record, description: nil)).to_not be_valid
-  end
-
-  it 'has a unique date' do
-    old_record = build(:record, date: Date.new.yesterday)
-    expect(build(:record).date).to_not eq(old_record.date)
+  context "validations" do
+    it { is_expected.to validate_presence_of :hours }
+    it { is_expected.to validate_presence_of :description }
+    it { is_expected.to validate_presence_of :date }
+    it { is_expected.to validate_uniqueness_of :date }
   end
 end
