033d35319f59ec8ad16b404b65e66f52f46c0585
Jaime Alvarez <jaime_andres@unitec.edu>
2017-04-06 09:50:22 +0000

    remove scope for uniqueness validation

--- app/models/record.rb
+++ app/models/record.rb
@@ -1,13 +1,13 @@
 class Record < ActiveRecord::Base
     belongs_to :punchcard
-    
+
     validates :date, :presence => true
-    validates :date, :uniqueness => { scope: :punchcard_id }
-    
+    validates :date, :uniqueness => true #{ scope: :punchcard_id }
+
     validates :hours, :presence => true
     validates :description, :presence => true
-    
-    
+
+
     def next
         self.class.where("id > ? AND punchcard_id = ?", id, self.punchcard_id).first
     end
@@ -16,5 +16,5 @@ class Record < ActiveRecord::Base
         self.class.where("id < ? AND punchcard_id = ?", id, self.punchcard_id).last
     end
 
-    
+
 end
