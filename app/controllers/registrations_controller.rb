45ad12fcd3af3c37599f89b708c601d6426dd773
Jaime Alvarez <jaime_andres@unitec.edu>
2018-08-09 03:36:26 +0000

    Fix sign up bug

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -5,6 +5,10 @@ class RegistrationsController < Devise::RegistrationsController
 
   protected
 
+  def configure_sign_up_params
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:time_zone])
+  end
+
   def configure_account_update_params
     devise_parameter_sanitizer.permit(:account_update, keys: [:time_zone])
   end
