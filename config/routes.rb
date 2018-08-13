94302dfa6f0920326ee2168ef59489d04bbbde4f
Jaime Alvarez <jaime_andres@unitec.edu>
2018-08-13 02:54:59 +0000

    Pricing plans page

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,4 @@
 Rails.application.routes.draw do
-  get 'errors/not_found'
-
-  get 'errors/internal_server_error'
-
   devise_for :users, controllers: { registrations: 'registrations' }
 
   devise_scope :user do
@@ -26,5 +22,8 @@ Rails.application.routes.draw do
   match '/404', :to => 'errors#not_found', :via => :all
   match '/500', :to => 'errors#internal_server_error', :via => :all
   match '/403', :to => 'errors#forbidden', :via => :all
+
+  # Static Pages
+  get 'pricing', to: 'home#pricing'
 
 end
